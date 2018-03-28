d0e808767b45204db7aff79fdf54f87bee6a6bdd
Aleksi Kallio <onik@onik.org>
2018-03-28 18:50:06 +0300

    Established cli params

--- index.js
+++ index.js
@@ -2,11 +2,48 @@
 let program = require("commander");
 
 program
-  .arguments("<preset>")
+  .version("1.0.0", "-v, --version");
+
+program
+  .command("token <token>")
+  .description("Saves your token")
+  .action((token) => {
+    console.log("Save token " + token);
+  });
+
+program
+  .command("preset <preset>")
+  .description("Uses the preset (can be listed with command list-presets)")
+  .action((preset) => {
+    console.log("Preset selected: " + preset);
+  });
+
+program
+  .command("default")
+  .description("Restores your default status (can be set with setdefault")
+  .action((preset) => {
+    console.log("Preset selected: " + preset);
+  });
+
+program
+  .command("list-presets")
+  .description("Lists the available presets")
+  .action(() => {
+    console.log("Defined presets: meeting, lunch");
+  });
+
+program
+  .command("setdefault")
+  .description("Sets the default status, use -e and -m to set")
+  .action((preset) => {
+    console.log("Preset selected: " + preset);
+  });
+
+program
   .option("-e, --emoji <emoji>", "Use custom emoji")
   .option("-m, --message <message>", "Use custom message")
-  .parse(process.argv);
 
+program.parse(process.argv);
 if (!process.argv.slice(2).length) {
   program.outputHelp();
 }
