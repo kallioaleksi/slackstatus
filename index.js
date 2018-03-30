17a4e79f3d6ccc47e7795cad502144f0f479cf5b
Aleksi Kallio <onik@onik.org>
2018-03-30 13:07:40 +0300

    Refactored argument parsing

--- index.js
+++ index.js
@@ -9,58 +9,11 @@ const DEBUG = true;
 
 let cfg;
 
-program
-  .version("1.0.0", "-v, --version");
-
-program
-  .command("token <token>")
-  .description("Saves your token")
-  .action((token) => {
-    console.log("Save token " + token);
-  });
-
-program
-  .command("preset <preset>")
-  .description("Uses the preset (can be listed with command list-presets)")
-  .action((preset) => {
-    console.log("Preset selected: " + preset);
-  });
-
-program
-  .command("default")
-  .description("Restores your default status (can be set with setdefault")
-  .action((preset) => {
-    console.log("Preset selected: " + preset);
-  });
-
-program
-  .command("list-presets")
-  .description("Lists the available presets")
-  .action(() => {
-    console.log("Defined presets: meeting, lunch");
-  });
-
-program
-  .command("setdefault")
-  .description("Sets the default status, use -e and -m to set")
-  .action((preset) => {
-    console.log("Preset selected: " + preset);
-  });
-
-program
-  .option("-e, --emoji <emoji>", "Use custom emoji")
-  .option("-m, --message <message>", "Use custom message")
-  .option("-c, --config <configfile>", "Define alternative config file")
-  .parse(process.argv);
-
-//program.parse(process.argv);
-if (!process.argv.slice(2).length) {
-  program.outputHelp();
-}
+let pr = parseArgs(process.argv);
 
 let configfile = path.resolve(os.homedir(), ".slackstatus");
-if(program.config) {
-  configfile = program.config;
+if(pr.config) {
+  configfile = pr.config;
 }
 
 loadConfig(configfile).then((data) => {
@@ -82,6 +35,44 @@ loadConfig(configfile).then((data) => {
 }).catch((err) => {
   console.log(err);
 });
+
+function parseArgs(args) {
+  program
+    .version("1.0.0", "-v, --version");
+
+  program.command("token <token>").description("Saves your token").action((token) => {
+    console.log("Save token " + token);
+  });
+
+  program.command("preset <preset>").description("Uses the preset (can be listed with command list-presets)").action((preset) => {
+    console.log("Preset selected: " + preset);
+  });
+
+  program.command("default").description("Restores your default status (can be set with setdefault").action((preset) => {
+    console.log("Preset selected: " + preset);
+  });
+
+  program.command("list-presets").description("Lists the available presets").action(() => {
+    console.log("Defined presets: meeting, lunch");
+  });
+
+  program.command("setdefault").description("Sets the default status, use -e and -m to set").action((preset) => {
+    console.log("Preset selected: " + preset);
+  });
+
+  program
+    .option("-e, --emoji <emoji>", "Use custom emoji")
+    .option("-m, --message <message>", "Use custom message")
+    .option("-c, --config <configfile>", "Define alternative config file")
+    .parse(args);
+
+  //program.parse(process.argv);
+  if (!args.slice(2).length) {
+    program.outputHelp();
+  }
+
+  return program;
+}
 
 function loadConfig(configFile) {
   return new Promise((resolve, reject) => {
