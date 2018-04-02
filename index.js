804e6017e1cc079ee95039a5534f70bfd9018f97
Aleksi Kallio <onik@onik.org>
2018-04-02 13:55:17 +0300

    Implemented token updating and restructured config handling

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let program = require("commander");
 let request = require("superagent");
 let path = require("path");
 let jsonfile = require("jsonfile");
+let co = require("co");
+let prompt = require("co-prompt");
 
 const os = require("os");
 const DEBUG = true;
 
-let cfg;
+let cfg, mode, providedToken, providedUsePreset;
 
 let pr = parseArgs(process.argv);
 
@@ -16,32 +18,36 @@ if(pr.config) {
   configfile = pr.config;
 }
 
-loadConfig(configfile).then((data) => {
-  console.log("Loaded config!");
-  cfg = data;
-}, (err) => {
-  if(DEBUG) console.log(err);
-  console.log("Error loading config, trying to create...");
-  let template = {"token": ""};
-  let createP = createConfig(configfile, template);
-  createP.then((config) => {
-    console.log("Config created successfully!");
-    cfg = config;
-  }, (err) => {
-    if(DEBUG) console.log(err);
-    console.log("Couldn't create configfile, quitting!");
-    process.exit(1);
-  });
-}).catch((err) => {
-  console.log(err);
+handleConfig(configfile).then((cfg) => {
+  switch(mode) {
+  case "token":
+    cfg.token = providedToken;
+    createOrUpdateConfig(configfile, cfg);
+    console.log("Saved new token!");
+    break;
+  default: 
+    console.log("No mode selected!");
+  }
 });
 
 function parseArgs(args) {
   program
     .version("1.0.0", "-v, --version");
 
-  program.command("token <token>").description("Saves your token").action((token) => {
-    console.log("Save token " + token);
+  program.command("token [token]").description("Saves your token").action((token) => {
+    if(!token) {
+      co(function *() {
+        let tmpToken = yield prompt("Token: ");
+        return tmpToken;
+      }).then((value) => {
+        providedToken = value;
+      }).then(() => {
+        process.stdin.pause();
+      });
+    } else {
+      providedToken = token;
+    }
+    mode = "token";
   });
 
   program.command("preset <preset>").description("Uses the preset (can be listed with command list-presets)").action((preset) => {
@@ -49,7 +55,7 @@ function parseArgs(args) {
   });
 
   program.command("default").description("Restores your default status (can be set with setdefault").action((preset) => {
-    console.log("Preset selected: " + preset);
+    console.log("Restoring default: " + cfg.default.emoji + " - " + cfg.default.message);
   });
 
   program.command("list-presets").description("Lists the available presets").action(() => {
@@ -57,7 +63,7 @@ function parseArgs(args) {
   });
 
   program.command("setdefault").description("Sets the default status, use -e and -m to set").action((preset) => {
-    console.log("Preset selected: " + preset);
+    console.log("Preset to set: " + emoji + " - " + message);
   });
 
   program
@@ -86,7 +92,7 @@ function loadConfig(configFile) {
   });
 }
 
-function createConfig(configFile, template) {
+function createOrUpdateConfig(configFile, template) {
   return new Promise((resolve, reject) => {
     jsonfile.writeFile(configFile, template, (err) => {
       if(err) {
@@ -94,6 +100,29 @@ function createConfig(configFile, template) {
       } else {
         resolve(template);
       }
+    });
+  });
+}
+
+function handleConfig(configfile) {
+  return new Promise((resolve, reject) => {
+    loadConfig(configfile).then((data) => {
+      console.log("Loaded config!");
+      resolve(data);
+    }, (err) => {
+      if(DEBUG) console.log(err);
+      console.log("Error loading config, trying to create...");
+      let template = {"token": ""};
+      createOrUpdateConfig(configfile, template).then((config) => {
+        console.log("Config created successfully!");
+        resolve(config);
+      }, (err) => {
+        if(DEBUG) console.log(err);
+        console.log("Couldn't create configfile, quitting!");
+        process.exit(1);
+      });
+    }).catch((err) => {
+      reject(err);
     });
   });
 }
