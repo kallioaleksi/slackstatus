0c016b36f5c4ad2358451fe04e6f0e0bdb013a9f
Aleksi Kallio <onik@onik.org>
2018-04-08 22:24:41 +0300

    Code formatting Let to const tweaking

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/local/bin/node
 
-let program = require("commander");
-let request = require("superagent");
-let path = require("path");
-let jsonfile = require("jsonfile");
-let co = require("co");
-let prompt = require("co-prompt");
-let clitable = require("cli-table");
+const program  = require("commander");
+const request  = require("superagent");
+const path     = require("path");
+const jsonfile = require("jsonfile");
+const co       = require("co");
+const prompt   = require("co-prompt");
+const clitable = require("cli-table");
+const os       = require("os");
 
-const os = require("os");
-const url = "https://slack.com/api/users.profile.set";
+const url   = "https://slack.com/api/users.profile.set";
 const DEBUG = false;
 
 let cfg, providedToken, presetVar;
@@ -19,7 +19,7 @@ let mode = null;
 let pr = parseArgs(process.argv);
 
 let configfile = path.resolve(os.homedir(), ".slackstatus");
-if(pr.config) {
+if (pr.config) {
   configfile = pr.config;
 }
 
@@ -34,50 +34,72 @@ handleConfig(configfile).then((returncfg) => {
 });
 
 function parseArgs(args) {
+  program.version("1.0.0", "-v, --version");
+
   program
-    .version("1.0.0", "-v, --version");
+    .command("token [token]")
+    .description("Saves your token")
+    .action((token) => {
+      mode = "token";
+      if (!token) {
+        co(function * () {
+          let tmpToken = yield prompt("Token: ");
+          return tmpToken;
+        }).then((value) => {
+          providedToken = value;
+        }).then(() => {
+          process
+            .stdin
+            .pause();
+        });
+      } else {
+        providedToken = token;
+      }
+    });
 
-  program.command("token [token]").description("Saves your token").action((token) => {
-    mode = "token";
-    if(!token) {
-      co(function *() {
-        let tmpToken = yield prompt("Token: ");
-        return tmpToken;
-      }).then((value) => {
-        providedToken = value;
-      }).then(() => {
-        process.stdin.pause();
-      });
-    } else {
-      providedToken = token;
-    }
-  });
+  program
+    .command("preset <preset>")
+    .description("Uses the preset (can be listed with command list-presets)")
+    .action((preset) => {
+      mode = "usepreset";
+      presetVar = preset;
+    });
 
-  program.command("preset <preset>").description("Uses the preset (can be listed with command list-presets)").action((preset) => {
-    mode = "usepreset";
-    presetVar = preset;
-  });
+  program
+    .command("default")
+    .description("Restores your default status (can be set with setdefault)")
+    .action(() => {
+      mode = "default";
+    });
 
-  program.command("default").description("Restores your default status (can be set with setdefault)").action(() => {
-    mode = "default";
-  });
+  program
+    .command("list-presets")
+    .description("Lists the available presets")
+    .action(() => {
+      mode = "list";
+    });
 
-  program.command("list-presets").description("Lists the available presets").action(() => {
-    mode = "list";
-  });
+  program
+    .command("save-preset <preset>")
+    .description("Saves the preset defined by -e and -m")
+    .action((preset) => {
+      mode = "save";
+      presetVar = preset;
+    });
 
-  program.command("save-preset <preset>").description("Saves the preset defined by -e and -m").action((preset) => {
-    mode = "save";
-    presetVar = preset;
-  });
+  program
+    .command("setdefault")
+    .description("Sets the default status, use -e and -m to set")
+    .action(() => {
+      mode = "setDefault";
+    });
 
-  program.command("setdefault").description("Sets the default status, use -e and -m to set").action(() => {
-    mode = "setDefault";
-  });
-
-  program.command("set").description("Sets the emoji and message provided with the -e and -m options").action(() => {
-    mode = "set";
-  });
+  program
+    .command("set")
+    .description("Sets the emoji and message provided with the -e and -m options")
+    .action(() => {
+      mode = "set";
+    });
 
   program
     .option("-e, --emoji <emoji>", "Use custom emoji")
@@ -85,11 +107,10 @@ function parseArgs(args) {
     .option("-c, --config <configfile>", "Define alternative config file")
     .parse(args);
 
-  //program.parse(process.argv);
   if (!args.slice(2).length) {
     program.outputHelp();
   }
-  if(mode === null) {
+  if (mode === null) {
     mode = "unset";
   }
   return program;
@@ -98,7 +119,7 @@ function parseArgs(args) {
 function loadConfig(configFile) {
   return new Promise((resolve, reject) => {
     jsonfile.readFile(configFile, (err, data) => {
-      if(err) {
+      if (err) {
         reject(err);
       } else {
         resolve(data);
@@ -110,7 +131,7 @@ function loadConfig(configFile) {
 function createOrUpdateConfig(configFile, template) {
   return new Promise((resolve, reject) => {
     jsonfile.writeFile(configFile, template, (err) => {
-      if(err) {
+      if (err) {
         reject(err);
       } else {
         resolve(template);
@@ -124,13 +145,17 @@ function handleConfig(configfile) {
     loadConfig(configfile).then((data) => {
       resolve(data);
     }, (err) => {
-      if(DEBUG) console.log(err);
+      if (DEBUG) 
+        console.log(err);
       console.log("Creating new configfile...");
-      let template = {"token": ""};
+      let template = {
+        "token": ""
+      };
       createOrUpdateConfig(configfile, template).then((config) => {
         resolve(config);
       }, (err) => {
-        if(DEBUG) console.log(err);
+        if (DEBUG) 
+          console.log(err);
         console.log("Couldn't create configfile, quitting!");
         process.exit(1);
       });
@@ -141,103 +166,100 @@ function handleConfig(configfile) {
 }
 
 function handleMode() {
-  switch(mode) {
-  case "token":
-    cfg.token = providedToken;
-    createOrUpdateConfig(configfile, cfg);
-    console.log("Saved new token!");
-    break;
-  case "list":
-    if(typeof cfg.presets !== "undefined" && cfg.presets.length > 0) {
-      console.log("Defined presets:");
-      let table = new clitable({
-        head: ["Preset name", "Emoji", "Message"]
-      });
-      for(let i in cfg.presets) {
-        table.push([
-          cfg.presets[i].name,
-          cfg.presets[i].emoji,
-          cfg.presets[i].message
-        ]);
-        //console.log(" - " + cfg.presets[i].name);
+  switch (mode) {
+    case "token":
+      cfg.token = providedToken;
+      createOrUpdateConfig(configfile, cfg);
+      console.log("Saved new token!");
+      break;
+    case "list":
+      if (typeof cfg.presets !== "undefined" && cfg.presets.length > 0) {
+        console.log("Defined presets:");
+        let table = new clitable({
+          head: ["Preset name", "Emoji", "Message"]
+        });
+        for (let i in cfg.presets) {
+          table.push([cfg.presets[i].name, cfg.presets[i].emoji, cfg.presets[i].message]);
+          //console.log(" - " + cfg.presets[i].name);
+        }
+        console.log(table.toString());
+      } else {
+        console.log("No presets found!");
       }
-      console.log(table.toString());
-    } else {
-      console.log("No presets found!");
-    }
-    break;
-  case "save":
-    if(typeof pr.message === "undefined" || typeof pr.emoji === "undefined") {
-      console.log("Missing preset argument(s) -e <emoji> or -m <message>!");
-      return;
-    }
-    if(typeof cfg.presets === "undefined") {
-      cfg.presets = [];
-    }
-    for(let i in cfg.presets) {
-      if(cfg.presets[i].name === presetVar) {
-        cfg.presets.splice(i, 1);
+      break;
+    case "save":
+      if (typeof pr.message === "undefined" || typeof pr.emoji === "undefined") {
+        console.log("Missing preset argument(s) -e <emoji> or -m <message>!");
+        return;
       }
-    }
-    if(pr.emoji.charAt(0) !== ":") {
-      pr.emoji = ":" + pr.emoji;
-    }
-    if(pr.emoji.slice(-1) !== ":") {
-      pr.emoji = pr.emoji + ":";
-    }
-    cfg.presets.push({
-      name: presetVar,
-      emoji: pr.emoji,
-      message: pr.message
-    });
-    createOrUpdateConfig(configfile, cfg);
-    console.log("Preset saved!");
-    break;
-  case "setDefault":
-    if(typeof pr.emoji === "undefined" || typeof pr.emoji === "undefined") {
-      console.log("Missing preset arguments -e <emoji> and -m <message>!");
-      return;
-    }
-    if(pr.emoji.charAt(0) !== ":") {
-      pr.emoji = ":" + pr.emoji;
-    }
-    if(pr.emoji.slice(-1) !== ":") {
-      pr.emoji = pr.emoji + ":";
-    }
-    cfg.default = {
-      name: presetVar,
-      emoji: pr.emoji,
-      message: pr.message
-    };
-    createOrUpdateConfig(configfile, cfg);
-    console.log("Default preset saved!");
-    break;
-  case "default":
-    updateStatus(cfg.token, cfg.default.emoji, cfg.default.message);
-    break;
-  case "usepreset":
-    for(let i in cfg.presets) {
-      if(cfg.presets[i].name === presetVar) {
-        updateStatus(cfg.token, cfg.presets[i].emoji, cfg.presets[i].message);
-        break;
+      if (typeof cfg.presets === "undefined") {
+        cfg.presets = [];
       }
-    }
-    break;
-  case "set":
-    if(typeof pr.emoji === "undefined" || typeof pr.emoji === "undefined") {
-      console.log("Missing preset arguments -e <emoji> and -m <message>!");
-      return;
-    }
-    if(pr.emoji.charAt(0) !== ":") {
-      pr.emoji = ":" + pr.emoji;
-    }
-    if(pr.emoji.slice(-1) !== ":") {
-      pr.emoji = pr.emoji + ":";
-    }
-    updateStatus(cfg.token, pr.emoji, pr.message);
-    break;
-  default: 
-    console.log("No mode selected!");
+      for (let i in cfg.presets) {
+        if (cfg.presets[i].name === presetVar) {
+          cfg
+            .presets
+            .splice(i, 1);
+        }
+      }
+      if (pr.emoji.charAt(0) !== ":") {
+        pr.emoji = ":" + pr.emoji;
+      }
+      if (pr.emoji.slice(-1) !== ":") {
+        pr.emoji = pr.emoji + ":";
+      }
+      cfg
+        .presets
+        .push({name: presetVar, emoji: pr.emoji, message: pr.message});
+      createOrUpdateConfig(configfile, cfg);
+      console.log("Preset saved!");
+      break;
+    case "setDefault":
+      if (typeof pr.emoji === "undefined" || typeof pr.emoji === "undefined") {
+        console.log("Missing preset arguments -e <emoji> and -m <message>!");
+        return;
+      }
+      if (pr.emoji.charAt(0) !== ":") {
+        pr.emoji = ":" + pr.emoji;
+      }
+      if (pr.emoji.slice(-1) !== ":") {
+        pr.emoji = pr.emoji + ":";
+      }
+      cfg.
+    default = {
+        name: presetVar,
+        emoji: pr.emoji,
+        message: pr.message
+      };
+      createOrUpdateConfig(configfile, cfg);
+      console.log("Default preset saved!");
+      break;
+    case "default":
+      updateStatus(cfg.token, cfg.default.emoji, cfg.default.message);
+      break;
+    case "usepreset":
+      for (let i in cfg.presets) {
+        if (cfg.presets[i].name === presetVar) {
+          updateStatus(cfg.token, cfg.presets[i].emoji, cfg.presets[i].message);
+          break;
+        }
+      }
+      break;
+    case "set":
+      if (typeof pr.emoji === "undefined" || typeof pr.emoji === "undefined") {
+        console.log("Missing preset arguments -e <emoji> and -m <message>!");
+        return;
+      }
+      if (pr.emoji.charAt(0) !== ":") {
+        pr.emoji = ":" + pr.emoji;
+      }
+      if (pr.emoji.slice(-1) !== ":") {
+        pr.emoji = pr.emoji + ":";
+      }
+      updateStatus(cfg.token, pr.emoji, pr.message);
+      break;
+    default:
+      console.log("No mode selected!");
   }
 }
 
@@ -252,9 +274,9 @@ function updateStatus(updateToken, updateEmoji, updateMessage) {
     .set("Content-type", "application/json; charset=utf-8")
     .set("Authorization", "Bearer " + updateToken)
     .end((err, response) => {
-      if(!err) {
-        if(typeof response.body !== "undefined") {
-          if(response.body.ok) {
+      if (!err) {
+        if (typeof response.body !== "undefined") {
+          if (response.body.ok) {
             console.log("Status updated successfully!");
           } else {
             console.log("Error: " + response.body.error);
